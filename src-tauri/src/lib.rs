ac9d870ac45fef8a03d2c01ac7649d48534cb06d
OfficialFreak <npfreak@t-online.de>
2025-04-09 22:44:05 +0200

    Actually install new updates

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,9 +18,39 @@ async fn close_splashscreen(window: Window) {
     };
 }
 
+async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    if let Some(update) = app.updater()?.check().await? {
+        let mut downloaded = 0;
+    
+        update
+            .download_and_install(
+            |chunk_length, content_length| {
+                downloaded += chunk_length;
+                println!("downloaded {downloaded} from {content_length:?}");
+            },
+            || {
+                println!("download finished");
+            },
+            )
+            .await?;
+    
+        println!("update installed");
+        app.restart();
+    }
+  
+    Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle().clone();
+            tauri::async_runtime::spawn(async move {
+            update(handle).await.unwrap();
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_fs::init())
