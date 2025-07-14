10c699349b165e55c19787b9efc5e6eac0ab6f4b
OfficialBrot <36410565+OfficialFreak@users.noreply.github.com>
2025-07-14 18:40:02 +0200

    High resolution images and video poster

--- src/lib/components/Display.svelte
+++ src/lib/components/Display.svelte
@@ -9,18 +9,15 @@
     } = $props();
 
     let hovering = $state(false);
-    let asset_name = $derived(
-        thumbnail || (hoverVideoOnly && !hovering)
-            ? asset + "_thumbnail.webp"
-            : asset,
-    );
-    let video = $derived(asset_name.endsWith(".mp4"));
+    let thumbnail_asset_name = $derived(asset + "_thumbnail.webp");
+    let video = $derived(asset.endsWith(".mp4"));
+    let still = $derived(video ? asset + "_still.webp" : asset);
 
-    let url = $derived(
-        appState.token
+    function toUrl(asset_name: string) {
+        return appState.token
             ? `${appState.secure ? "https://" : "http://"}${appState.baseUrl}/assets/${asset_name}?key=${encodeURIComponent(appState.token)}`
-            : "",
-    );
+            : "";
+    }
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
@@ -28,18 +25,32 @@
     class="contents"
     onmouseenter={() => {
         hovering = true;
-        console.log("Hovering");
     }}
     onmouseleave={() => {
         hovering = false;
-        console.log("Hovering ended");
     }}
 >
-    {#if url}
-        {#if video}
-            <video autoplay loop src={url} {...props}></video>
+    {#if thumbnail}
+        {#if toUrl(thumbnail_asset_name)}
+            <img src={toUrl(thumbnail_asset_name)} {...props} />
         {:else}
-            <img src={url} {...props} />
+            <div {...props}></div>
+        {/if}
+    {:else if toUrl(asset)}
+        {#if video}
+            {#if !hoverVideoOnly || hovering}
+                <video
+                    autoplay
+                    loop
+                    src={toUrl(asset)}
+                    poster={toUrl(still)}
+                    {...props}
+                ></video>
+            {:else}
+                <img src={toUrl(still)} {...props} />
+            {/if}
+        {:else}
+            <img src={toUrl(asset)} {...props} />
         {/if}
     {:else}
         <div {...props}></div>
