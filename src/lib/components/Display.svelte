8d0bc45333a8e069fa2dc6d97546d0dade736694
OfficialFreak <npfreak@t-online.de>
2025-07-24 14:19:07 +0200

    Don't time out constantly looping video

--- src/lib/components/Display.svelte
+++ src/lib/components/Display.svelte
@@ -14,19 +14,6 @@
     let still = $derived(video ? asset + "_still.webp" : asset);
 
     let video_element: HTMLVideoElement | null = $state(null);
-    let video_timeout = $state(-1);
-
-    // Timing out video because sometimes the mouseleave event doesn't fire (Doesn't time out when cursor stays on - for some reason lmao)
-    function setVideoTimeout() {
-        if (video_timeout != -1) {
-            clearTimeout(video_timeout);
-        }
-        if (hovering && video_element) {
-            video_timeout = setTimeout(() => {
-                hovering = false;
-            }, video_element.duration * 1000);
-        }
-    }
 
     function toUrl(asset_name: string) {
         return appState.token
@@ -48,7 +35,6 @@
             {#if !hoverVideoOnly || hovering}
                 <!-- onloadeddata doesn't fire on mobile with data-saver enabled (code is removed on mobile) -->
                 <video
-                    onloadeddata={setVideoTimeout}
                     bind:this={video_element}
                     autoplay
                     loop
